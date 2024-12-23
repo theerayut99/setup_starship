348ba735c23cfdce0e71d024fd8f911f492332f3
Theerayut Thanyaphoo <theerayut_than@hotmail.com>
2024-12-23 11:31:35 +0700

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# Set Starship init fish to default shell
+starship init fish | source
+# nvm use default node version
+nvm use default
